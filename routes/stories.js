cb26d8bc6701481e300c0098d5cef3a7a0db1fb8
Joel Elliston <lachre@outlook.com>
2020-07-03 18:53:02 +0300

    create add story view and link route

---
+++ routes/stories.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth } = require('../middleware/auth')  // ../ = 1st navigating up one level 
+
+const Story = require('../models/Story')
+
+//@desc         Show add page
+//@route        GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+    //send text to client
+    res.render('stories/add.hbs')
+})
+
+
+module.exports = router
