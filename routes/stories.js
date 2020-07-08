52aadd170b99abfb4e90b30b46f914b5776fb7ce
Joel Elliston <lachre@outlook.com>
2020-07-08 14:51:42 +0300

    method override for DELETE requests, icons for edit and del, route refactoring

--- routes/stories.js
+++ routes/stories.js
@@ -49,29 +49,37 @@ router.get('/', ensureAuth, async (req, res) => {    //needs to be async since w
 //@desc         Show edit page
 //@route        GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-    // find one story whose id matches the id of the currently logged in user
-    const story = await Story.findOne({
+    try {
+      // find one story whose id matches the id of the currently logged in user
+      const story = await Story.findOne({
         _id: req.params.id
-    }).lean()
+      }).lean()
     
-    //check to see if the story is there
-    if (!story) {
+      //check to see if the story is there
+      if (!story) {
         return res.render('error/404.hbs')
-    }
+      }
 
-    // we don't want users to be able to edit stories that aren't theirs
-    if (story.user != req.user.id) {
+      // we don't want users to be able to edit stories that aren't theirs
+      if (story.user != req.user.id) {
         res.redirect('/stories')
-    } else {
+      } else {
         res.render('stories/edit.hbs', {
             story,
         })
+      }  
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500.hbs') 
     }
+    
 })
 
 //@desc         Update story
 //@route        PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
+
+  try {  
     //check to see if the story is there (req.params.id gets the id from the url)
     let story = await Story.findById(req.params.id).lean()
     
@@ -97,6 +105,22 @@ router.put('/:id', ensureAuth, async (req, res) => {
         //redirect to dashboard when finished
         res.redirect('/dashboard')
     }
+  } catch (err) {
+        console.error(err)
+        return res.render('error/500.hbs')
+  }
+})
+
+//@desc         Delete story
+//@route        DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Story.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500.hbs')
+    }    
 })
 
 module.exports = router
