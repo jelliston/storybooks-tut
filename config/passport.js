190c43e6a85eef7a46dd1649e9d5bc354ab01523
Joel Elliston <lachre@outlook.com>
2020-07-15 18:58:44 +0300

    debug azure auth, removed image from newUser, now working

--- config/passport.js
+++ config/passport.js
@@ -53,8 +53,7 @@ module.exports = function (passport) {
                 googleId: profile.id,
                 displayName: profile.displayName,
                 firstName: profile.name.givenName,
-                lastName: profile.name.familyName,
-                image: profile.photos[0].value
+                lastName: profile.name.familyName
             }
 
             try {
